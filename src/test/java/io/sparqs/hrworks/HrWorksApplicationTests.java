e06041730dea5d8f094e730c1a43572e49281dc7
Leon Jäkel <leon.jaekel@sparqs.io>
2021-02-19 16:07:04 +0100

    Disable integration tests

--- src/test/java/io/sparqs/hrworks/HrWorksApplicationTests.java
+++ src/test/java/io/sparqs/hrworks/HrWorksApplicationTests.java
@@ -1,8 +1,10 @@
 package io.sparqs.hrworks;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class HrWorksApplicationTests {
 
